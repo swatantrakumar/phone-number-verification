c9c1f0c1009a71ca9eed44bfe7bd28119544bc85
Swatantra Kumar <rajswatantra9@gmail.com>
2024-12-06 13:08:26 +0530

    update code for some chagnes

--- index.js
+++ index.js
@@ -2,13 +2,10 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const loadApi = require('./server/api/api');
 
-
-
-
 const connectToDatabase = async () => {
   try {
     const DB_HOST = process.env.DB_HOST || 'mongodb://127.0.0.1:27017';
-    const DB_NAME = process.env.DB_NAME || 'central-elabs-prod';
+    const DB_NAME = process.env.DB_NAME || 'test';
     await mongoose.connect(DB_HOST,{"dbName":DB_NAME});
     loadApi();
     console.log('Connected successfully to MongoDB');
